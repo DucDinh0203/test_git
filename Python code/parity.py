215d4241a459ce29fa9ac1ca7d71ec661cdee192
Duc Dinh <fxdinhtriduc@gmail.com>
2021-07-27 22:47:18 +1000

    add some comment

--- Python code/parity.py
+++ Python code/parity.py
@@ -1,12 +1,15 @@
 def parity(lst):
     p = 0
-    i = 1
+    i = 0
     n = len(lst) - 1
+    # the use invariant is p as a parity of lst
     while i <= n:
         if lst[i] % 2 == 1:
             p = not p
+            print(p)
         i += 1
     return p
 
 my_list = [1, 2, 3, 4, 5, 6]
 print(parity(my_list))
+print(not 0)
